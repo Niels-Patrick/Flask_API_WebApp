4ff7c4f02e1c42ec2d3e7502ee890bd3380243b5
Niels-Patrick <73878385+Niels-Patrick@users.noreply.github.com>
2024-11-14 10:35:23 +0100

    adding __init__.py

--- app/webapp.py
+++ app/webapp.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from . import DAL
+import DAL
 
 app = Flask(__name__) # Creating flask object
 app.config['DEBUG'] = True # Launching the debugger
